0f69e75bc715b0ad30a701d8723ae8bee436b087
Fábio Cruz <fabio.carvalho.cruz@gmail.com>
2025-10-15 17:41:02 +0100

    fixing logic

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -4,16 +4,19 @@ int	ft_isalpha(char *s)
 {
 	while(*s)
 	{
-		if (*s < 'a' && *s > 'z' || *s < 'A' && *s > 'Z')
-			return (0);
+		if (!(*s >= 'a' && *s <= 'z' || *s >= 'A' && *s <= 'Z'))
+			return 0;
 		s++;
 	}
-	return (1);
+	return 1;
 }
 
 int	main(void)
 {
 	printf("%d\n", ft_isalpha("Hello"));
-        printf("%d\n", ft_isalpha("45"));
-        printf("%d\n", ft_isalpha("World"));
+    printf("%d\n", ft_isalpha("45"));
+	printf("%d\n", ft_isalpha("World"));
+	printf("%d\n", ft_isalpha("!"));
+	printf("%d\n", ft_isalpha("_~"));
+	printf("%d\n", ft_isalpha("g"));
 }
